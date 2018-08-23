30b19e287c42ccac140f50e004925118935309dc
NatalliaK <studina-natali@yandex.by>
2018-08-23 15:07:48 +0300

    форматирование

--- api.js
+++ api.js
@@ -1,19 +1,19 @@
 function getDataByFetch(url) {
-	return fetch(`${url}`, {
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-		},
-	})
-		.then(req => {
-			if (req.status >= 200 && req.status < 300) {
-				return Promise.resolve(req);
-			} else {
-				return Promise.reject(new Error(req.statusText));
-			}
-		})
-		.then(req => req.json())
-		.catch(err => {
-			return `'Error: ', ${err}`;
-		});
+  return fetch(`${url}`, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(req => {
+      if (req.status >= 200 && req.status < 300) {
+        return Promise.resolve(req);
+      } else {
+        return Promise.reject(new Error(req.statusText));
+      }
+    })
+    .then(req => req.json())
+    .catch(err => {
+      return `'Error: ', ${err}`;
+    });
 }
